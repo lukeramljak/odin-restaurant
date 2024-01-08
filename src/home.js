88c85d4a45ddcb02e7f927c7c9a301466a5a81b2
Luke ramljak <lukeramljak@proton.me>
2024-01-08 22:41:50 +0800

    wip: update home

--- src/home.js
+++ src/home.js
@@ -1,28 +1,22 @@
-import Image from "./assets/pexels-cup-of-couple-8472650.jpg";
-
 export const addHomeContent = () => {
   const contentDiv = document.getElementById("content");
   contentDiv.innerHTML = "";
 
-  const containerDiv = document.createElement("section");
+  const container = document.createElement("section");
+  container.className = "home-container";
 
-  const h1 = document.createElement("h1");
-  h1.textContent = "Savour the Soul of Japan";
-  containerDiv.appendChild(h1);
+  container.innerHTML = `
+    <div class="hero">
+      <h1>Savour the Soul of Japan</h1>
+      <p>Indulge in a Symphony of Flavors with Handcrafted Noodles, Rich Broths, and Irresistible Toppings – Your Culinary Journey Begins Here!</p>
+      <button class="btn">Place Order</button>
+    </div>
+    <div>
+      <h2>Welcome to Ramen House</h2>
+      <p>At Ramen House, we invite you on a culinary journey through the heart of Japan, where each bowl is a celebration of tradition and flavor. Step into our cozy haven and experience the artistry of ramen, crafted with passion and served with warmth.</p>
+      <p>Dive into a world of tantalizing choices, from the rich and savory Hakata Classic Tonkotsu Ramen to the bold and spicy Karai Miso Temptation. Our menu is a symphony of tastes, each dish carefully curated to satisfy the most discerning palates. Whether you're a ramen connoisseur or a first-time adventurer, there's a bowl that awaits your discovery.</p>
+    </div>
+  `;
 
-  const p = document.createElement("p");
-  p.textContent =
-    "Indulge in a Symphony of Flavors with Handcrafted Noodles, Rich Broths, and Irresistible Toppings – Your Culinary Journey Begins Here!";
-  containerDiv.appendChild(p);
-
-  const img = document.createElement("img");
-  img.src = Image;
-  img.className = "hero";
-
-  const btn = document.createElement("button");
-  btn.textContent = "Place Order";
-  containerDiv.appendChild(btn);
-
-  contentDiv.appendChild(containerDiv);
-  contentDiv.appendChild(img);
+  contentDiv.appendChild(container);
 };
